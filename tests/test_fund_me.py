5afb6ed30b63797455511725d49582506359c4b2
ethkee <ethkee@keemail.me>
2022-04-20 03:42:35 -0500

    first commit

---
+++ tests/test_fund_me.py
@@ -0,0 +1,26 @@
+from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIROMENTS
+from scripts.deploy import deploy_fund_me
+from brownie import network, accounts, exceptions
+import pytest
+
+
+def test_can_fund_and_withdraw():
+    account = get_account()
+    fund_me = deploy_fund_me()
+    entrance_fee = fund_me.getEntranceFee() + 100
+    tx = fund_me.fund({"from": account, "value": entrance_fee})
+    tx.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee
+    tx2 = fund_me.withdraw({"from": account})
+    tx2.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == 0
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIROMENTS:
+        pytest.skip("only for local test network")
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    #  "gas_limit": 100000, "allow_revert": True
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
